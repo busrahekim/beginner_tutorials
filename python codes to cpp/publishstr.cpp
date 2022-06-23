6987fb152f4e6b1eab6564a9e390c2d5f6776ef3
Büşra Hekimoğlu <45571159+busrahekim@users.noreply.github.com>
2022-06-23 12:22:40 +0300

    python to cpp

---
+++ python codes to cpp/publishstr.cpp
@@ -0,0 +1,37 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
+
+using namespace std;
+
+
+int main(int argc, char** argv){
+
+   ros::init(argc, argv, "stringpublisher");
+   ros::NodeHandle n;
+   ros::Publisher wordpub = n.advertise<std_msgs::String>("/words", 10);
+
+   ros::Rate myrate(10);
+   int count = 0;
+   while(ros::ok()){
+	
+	std_msgs::String message;
+	stringstream ss;
+	ss << "Helloooo: " << count;
+	message.data = ss.str();
+	//ROS_INFO(...)
+	//cout << ...;
+	wordpub.publish(message);
+	myrate.sleep();
+
+	
+
+
+	count++;
+
+   }  
+
+
+
+   return 0;
+}
